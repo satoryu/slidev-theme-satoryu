6d57f6b66861e6c06a43c85da695a49f053059e1
Tatsuya Sato <satoryu.1981@gmail.com>
2022-10-08 18:02:00 +0900

    Initial Commit

---
+++ components/BaseLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="slidev-layout cover">
+    <div class="my-auto w-full">
+      <slot />
+    </div>
+    <div class="logo rounded-lg absolute bottom-0 right-0 m-1 w-10 h-10"></div>
+  </div>
+</template>
+
+<style lang="postcss">
+
+.logo {
+  background-image: url("../assets/logo.png");
+  background-repeat: no-repeat;
+  background-size: contain;
+  opacity: 0.7;
+}
+
+</style>
---
+++ layouts/intro.vue
@@ -0,0 +1,16 @@
+<template>
+  <base-layout>
+    <slot />
+  </base-layout>
+</template>
+
+<script>
+import BaseLayout from '../components/BaseLayout.vue'
+
+export default {
+  components: { BaseLayout },
+  setup() {
+
+  },
+}
+</script>
